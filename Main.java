77a056d5c71b3c1fcb369b924a976e57354bec57
Teodor Ivanov <135324409+RedPlum420@users.noreply.github.com>
2025-06-02 10:11:37 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new RPGItemCreator();
+        frame.setVisible(true);
+    }
+
+    public static String randomizeName(){
+        String name = "";
+        return name;
+    }
+}
